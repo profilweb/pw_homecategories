1dcfc7e96a82a6f789eb52fbf4e58dc34899f945
Manu.L <manu@profil-web.fr>
2021-12-21 10:43:52 +0100

    add translations

---
+++ translations/fr.php
@@ -0,0 +1,8 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{pw_homecategories}prestashop>pw_homecategories_0ec82d2a1c0ff151de25b2af1b783405'] = 'PW Catégories sur la HomePage';
+$_MODULE['<{pw_homecategories}prestashop>pw_homecategories_7c3e60044dc86e7657ac5e85a7b101fc'] = 'Affiche un bloc catégories sur la page d\'accueil';
+$_MODULE['<{pw_homecategories}prestashop>pw_homecategories_3e480dfc32f25db062e28fa513003957'] = 'Nos catégories principales';
+$_MODULE['<{pw_homecategories}prestashop>pw_homecategories_87bdf2b1731f2c475caf4afc3eae14d0'] = 'Pas de catégories';
